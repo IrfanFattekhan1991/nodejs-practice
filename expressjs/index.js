3c8b205b624cb1206f8b80caf035bf7e5c6065ea
IrfanFattekhan <irfan.fattekhan1991@gmail.com>
2024-01-25 18:18:24 +0530

    user input validation using joi

--- expressjs/index.js
+++ expressjs/index.js
@@ -1,17 +1,57 @@
+const Joi = require("joi");
 const express = require("express");
 
 const app = express();
+
+app.use(express.json());
+
+const courses = [
+  {
+    id: 1,
+    name: "course1",
+  },
+  {
+    id: 2,
+    name: "course2",
+  },
+  {
+    id: 3,
+    name: "course3",
+  },
+];
 
 app.get("/", (req, res) => {
   res.send("hellow world!!");
 });
 
 app.get("/api/courses", (req, res) => {
-  res.send([1, 2, 3]);
+  res.send(courses);
 });
 
-app.get("/api/courses/:year/:month", (req, res) => {
-  res.send(req.query);
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send("Course with the given ID not found");
+  res.send(course);
+});
+
+app.post("/api/courses", (req, res) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+
+  const result = schema.validate(req.body);
+
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+  res.send(course);
 });
 
 const port = process.env.PORT || 3000;
