6ea2aae94bbcf7b3109e72504c5023835965bb8d
IrfanFattekhan <irfan.fattekhan1991@gmail.com>
2024-01-26 17:23:29 +0530

    middleware examples

--- expressjs/index.js
+++ expressjs/index.js
@@ -1,9 +1,17 @@
 const Joi = require("joi");
+const logger = require("./logger");
 const express = require("express");
+const authenticate = require("./Authentication");
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.use(logger);
+
+app.use(authenticate);
 
 const courses = [
   {
