27271405e2aa6fca895f10dfdc0356ae824b717b
IrfanFattekhan <irfan.fattekhan1991@gmail.com>
2024-01-31 23:21:32 +0530

    vidly-api

--- vidly/index.js
+++ vidly/index.js
@@ -1,3 +1,4 @@
+const config = require("config");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const mongoose = require("mongoose");
@@ -5,11 +6,27 @@ const genres = require("./routes/genres");
 const customers = require("./routes/customers");
 const movies = require("./routes/movies");
 const rentals = require("./routes/rentals");
+const users = require("./routes/users");
+const auth = require("./routes/auth");
 const express = require("express");
 const app = express();
 
+// if (process.env.vidly_jwtPrivateKey !== "undefined") {
+//   console.log("It is set!", process.env.vidly_jwtPrivateKey);
+// } else {
+//   console.log("No set!");
+// }
+
+// if (!config.get("jwtPrivateKey")) {
+//   console.error("FATAL ERROR - jwtPrivateKey is not defined");
+//   process.exit(1);
+// }
+
 mongoose
-  .connect("mongodb://localhost/vidly")
+  .connect("mongodb://localhost:27017/vidly", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("Connected to MongoDB..."))
   .catch((err) => console.error("Could not connect to MongoDB...", err));
 
@@ -18,6 +35,8 @@ app.use("/api/genres", genres);
 app.use("/api/customers", customers);
 app.use("/api/movies", movies);
 app.use("/api/rentals", rentals);
+app.use("/api/users", users);
+app.use("/api/auth", auth);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
---
+++ vidly/routes/auth.js
@@ -0,0 +1,33 @@
+const config = require("config");
+const Joi = require("joi");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const { User } = require("../models/user");
+const mongoose = require("mongoose");
+const express = require("express");
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].messag);
+
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("Invalid username or password.");
+
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) res.status(400).send("Invalid username or password.");
+
+  const token = jwt.sign({ id: user._id }, "jwtAuthKey");
+  res.send(token);
+});
+
+async function validate(req) {
+  const schema = {
+    email: Joi.string().min(5).max(55).required().email(),
+    password: Joi.string().min(5).max(255).required(),
+  };
+
+  return Joi.validate(req, schema);
+}
+
+module.exports = router;
